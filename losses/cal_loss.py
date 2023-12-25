90694cb9eb41a64e2719ca43f3c9118543664a03
SunJ1025 <hill1.sunj@gmail.com>
2023-12-25 09:59:40 +0800

    update

---
+++ losses/cal_loss.py
@@ -0,0 +1,43 @@
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch
+
+# 计算各种损失函数
+# 输入为 特征和标签  返回 loss值
+
+
+def cal_loss(outputs, labels, loss_func):
+    loss = 0
+    if isinstance(outputs, list):
+        for i in outputs:
+            loss += loss_func(i, labels)
+        loss = loss / len(outputs)
+    else:
+        loss = loss_func(outputs, labels)
+    return loss
+
+
+def cal_kl_loss(outputs, outputs2, loss_func):
+    loss = 0
+    if isinstance(outputs, list):
+        for i in range(len(outputs)):
+            loss += loss_func(F.log_softmax(outputs[i], dim=1), F.softmax(Variable(outputs2[i]), dim=1))
+        loss = loss / len(outputs)
+    else:
+        loss = loss_func(F.log_softmax(outputs, dim=1), F.softmax(Variable(outputs2), dim=1))
+    return loss
+
+
+def cal_triplet_loss(outputs, outputs2, labels, labels3, loss_func):
+    if isinstance(outputs, list):
+        loss = 0
+        for i in range(len(outputs)):
+            out_concat = torch.cat((outputs[i], outputs2[i]), dim=0)
+            labels_concat = torch.cat((labels, labels3), dim=0)
+            loss += loss_func(out_concat, labels_concat)
+        loss = loss / len(outputs)
+    else:
+        out_concat = torch.cat((outputs, outputs2), dim=0)
+        labels_concat = torch.cat((labels, labels3), dim=0)
+        loss = loss_func(out_concat, labels_concat)
+    return loss
